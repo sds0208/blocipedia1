36dd22bc08a7cd3d04f9c3f73330d97562bed99c
sds0208 <sarahsantacruz86@gmail.com>
2017-12-10 02:52:04 +0000

    updated routes again

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   
   get 'downgrade' => 'charges#downgrade'
   
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations'}
   
   resources :users
 
