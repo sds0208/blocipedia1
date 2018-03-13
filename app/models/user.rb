3cf91750379b1b5125e13b5b769597c12f6bb26c
sds0208 <sarahsantacruz86@gmail.com>
2018-03-13 18:48:49 +0000

    mailer changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :wikis
   has_many :collaborators, dependent: :destroy
   has_many :collaborations, through: :collaborators, source: :wiki
